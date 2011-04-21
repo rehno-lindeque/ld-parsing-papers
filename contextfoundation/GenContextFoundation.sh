41ee9258dfddf68d0815617c741f434bc0659d78
Rehno Lindeque <errantkid@gmail.com>
2011-04-21 16:30:57 +0200

    Removed unrelated files from the repository

--- contextfoundation/GenContextFoundation.sh
+++
@@ -1,5 +0,0 @@
-latex contextfoundation
-bibtex contextfoundation
-latex contextfoundation
-pdflatex contextfoundation.tex
-evince contextfoundation.pdf
--- langlangspec/GenLangLangSpec.sh
+++
@@ -1,5 +0,0 @@
-latex langlangspec01
-bibtex langlangspec01
-latex langlangspec01
-pdflatex langlangspec01.tex
-evince langlangspec01.pdf
