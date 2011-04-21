87a7e248e538ee7091f9d70c1948a977e8c5b2bc
Rehno Lindeque <errantkid@gmail.com>
2011-04-21 16:48:31 +0200

    Renaming the new paper to "ldparsing" (a follow-up paper would most probably be called "gldparsing" for generalized left-to-right parsing with delayed reductions)

---
+++ ldparsing/build.sh
@@ -0,0 +1,11 @@
+rm ldparsing.aux
+latex ldparsing
+bibtex ldparsing
+latex ldparsing
+pdflatex ldparsing.tex
+evince ldparsing.pdf
+rm ldparsing.aux
+rm ldparsing.bbl
+rm ldparsing.blg
+rm ldparsing.log
+rm ldparsing.dvi
